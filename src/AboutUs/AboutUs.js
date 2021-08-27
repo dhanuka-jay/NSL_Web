2ec5332d714e348b0e28eb45a820a1bb1e458fa4
Dan Ilandarage <dhanuka.singhe@gmail.com>
2021-08-27 12:40:36 +1000

    bg_vid altered

--- src/AboutUs/AboutUs.js
+++ src/AboutUs/AboutUs.js
@@ -7,7 +7,7 @@ const AboutUs = () => {
             <div className="president-section">
                 <div className="photo-circle"></div>
                 <h2>President's Message</h2>
-                <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, 
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, 
                     exercitationem. Dolorem molestiae aliquam, autem fugiat maiores porro 
                     numquam impedit expedita cupiditate ab id molestias? Sint vero similique 
                     ab consectetur commodi dolorem iure quo, dolorum assumenda hic ut enim 
@@ -15,7 +15,7 @@ const AboutUs = () => {
                     quae maxime. Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                     In quibusdam possimus et qui quasi. Voluptate culpa harum beatae ipsa, 
                     nostrum accusantium inventore perferendis, facilis cupiditate itaque vero 
-                    adipisci sunt odio!</h4>
+                    adipisci sunt odio!</p>
             </div>
             <Divider horizontal>
                 <Header as='h2'>
@@ -25,41 +25,38 @@ const AboutUs = () => {
             <div className="club-history">
                 <div className="photo-circle"></div>
                 <h2 className="history-header">Club History in a nutshell</h2>
-                <p>
-                    <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum tempore 
-                        corporis incidunt voluptatibus eum fuga nostrum hic aliquid asperiores mollitia 
-                        aliquam voluptas recusandae explicabo veritatis sint amet sequi laboriosam, 
-                        blanditiis accusamus ut officiis quaerat adipisci ad? Nisi saepe doloribus 
-                        quaerat consequatur praesentium, officiis quibusdam, atque quo, incidunt ullam aperiam 
-                        magni error? Laboriosam quibusdam rem, repellat qui ut ex quod distinctio sapiente 
-                        assumenda maxime saepe tenetur debitis veniam nesciunt recusandae, nam veritatis 
-                        provident necessitatibus suscipit facere sunt, quae est iusto. Quis asperiores quia 
-                        veritatis adipisci inventore nisi. Iure eum aliquid, veritatis, error repudiandae 
-                        repellat vero natus perspiciatis libero deleniti magni consequuntur!s</h4>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum tempore 
+                    corporis incidunt voluptatibus eum fuga nostrum hic aliquid asperiores mollitia 
+                    aliquam voluptas recusandae explicabo veritatis sint amet sequi laboriosam, 
+                    blanditiis accusamus ut officiis quaerat adipisci ad? Nisi saepe doloribus 
+                    quaerat consequatur praesentium, officiis quibusdam, atque quo, incidunt ullam aperiam 
+                    magni error? Laboriosam quibusdam rem, repellat qui ut ex quod distinctio sapiente 
+                    assumenda maxime saepe tenetur debitis veniam nesciunt recusandae, nam veritatis 
+                    provident necessitatibus suscipit facere sunt, quae est iusto. Quis asperiores quia 
+                    veritatis adipisci inventore nisi. Iure eum aliquid, veritatis, error repudiandae 
+                    repellat vero natus perspiciatis libero deleniti magni consequuntur!s
                 </p>
-                <p>
-                    <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum tempore 
-                        corporis incidunt voluptatibus eum fuga nostrum hic aliquid asperiores mollitia 
-                        aliquam voluptas recusandae explicabo veritatis sint amet sequi laboriosam, 
-                        blanditiis accusamus ut officiis quaerat adipisci ad? Nisi saepe doloribus 
-                        quaerat consequatur praesentium, officiis quibusdam, atque quo, incidunt ullam aperiam 
-                        magni error? Laboriosam quibusdam rem, repellat qui ut ex quod distinctio sapiente 
-                        assumenda maxime saepe tenetur debitis veniam nesciunt recusandae, nam veritatis 
-                        provident necessitatibus suscipit facere sunt, quae est iusto. Quis asperiores quia 
-                        veritatis adipisci inventore nisi. Iure eum aliquid, veritatis, error repudiandae 
-                        repellat vero natus perspiciatis libero deleniti magni consequuntur!s</h4>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum tempore 
+                    corporis incidunt voluptatibus eum fuga nostrum hic aliquid asperiores mollitia 
+                    aliquam voluptas recusandae explicabo veritatis sint amet sequi laboriosam, 
+                    blanditiis accusamus ut officiis quaerat adipisci ad? Nisi saepe doloribus 
+                    quaerat consequatur praesentium, officiis quibusdam, atque quo, incidunt ullam aperiam 
+                    magni error? Laboriosam quibusdam rem, repellat qui ut ex quod distinctio sapiente 
+                    assumenda maxime saepe tenetur debitis veniam nesciunt recusandae, nam veritatis 
+                    provident necessitatibus suscipit facere sunt, quae est iusto. Quis asperiores quia 
+                    veritatis adipisci inventore nisi. Iure eum aliquid, veritatis, error repudiandae 
+                    repellat vero natus perspiciatis libero deleniti magni consequuntur!s
                 </p>
-                <p>
-                    <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum tempore 
-                        corporis incidunt voluptatibus eum fuga nostrum hic aliquid asperiores mollitia 
-                        aliquam voluptas recusandae explicabo veritatis sint amet sequi laboriosam, 
-                        blanditiis accusamus ut officiis quaerat adipisci ad? Nisi saepe doloribus 
-                        quaerat consequatur praesentium, officiis quibusdam, atque quo, incidunt ullam aperiam 
-                        magni error? Laboriosam quibusdam rem, repellat qui ut ex quod distinctio sapiente 
-                        assumenda maxime saepe tenetur debitis veniam nesciunt recusandae, nam veritatis 
-                        provident necessitatibus suscipit facere sunt, quae est iusto. Quis asperiores quia 
-                        veritatis adipisci inventore nisi. Iure eum aliquid, veritatis, error repudiandae 
-                        repellat vero natus perspiciatis libero deleniti magni consequuntur!s</h4>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum tempore 
+                    corporis incidunt voluptatibus eum fuga nostrum hic aliquid asperiores mollitia 
+                    aliquam voluptas recusandae explicabo veritatis sint amet sequi laboriosam, 
+                    blanditiis accusamus ut officiis quaerat adipisci ad? Nisi saepe doloribus 
+                    quaerat consequatur praesentium, officiis quibusdam, atque quo, incidunt ullam aperiam 
+                    magni error? Laboriosam quibusdam rem, repellat qui ut ex quod distinctio sapiente 
+                    assumenda maxime saepe tenetur debitis veniam nesciunt recusandae, nam veritatis 
+                    provident necessitatibus suscipit facere sunt, quae est iusto. Quis asperiores quia 
+                    veritatis adipisci inventore nisi. Iure eum aliquid, veritatis, error repudiandae 
+                    repellat vero natus perspiciatis libero deleniti magni consequuntur!s
                 </p>
             </div>
             
