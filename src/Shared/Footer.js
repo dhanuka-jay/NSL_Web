3e6277b98fb4a08ad239cd90a54d25789d5b4f9d
Dan Ilandarage <dhanuka.singhe@gmail.com>
2021-10-20 16:27:38 +1100

    footer altered

--- src/Shared/Footer.js
+++ src/Shared/Footer.js
@@ -1,16 +1,50 @@
-import { Image } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { Image, Icon } from 'semantic-ui-react';
 import FooterLogo from '../png_logo_NSL_70px.png';
+import './Shared.css';
 
 const Footer = () => {
     return (
         <div className="footer-container">
             <div className="footer-overlay"></div>
-            <div className="footer-logo-container">
+            <div className="logo-copyright-container">
                 <Image src={FooterLogo} alt="Player Profile Photo" /> 
-            </div>  
-            <div className="copyright">
-                <h4>Copyright &copy;2021, All Right Reserved</h4>
-            </div>   
+                <div className="text-wrap">
+                    <h4>&copy;2021 Northside Legends</h4>
+                    <h4>All Right Reserved</h4>
+                </div>                 
+            </div> 
+            <div className="footer-contacts">
+                <div className="contact-us">
+                    <Link to='/contacts'><p className="p-desc">Contact Us</p></Link>
+                </div>
+                <div className="contact-socialmedia-container">
+                    <a href="https://www.facebook.com/NorthSide-Legends-540287353026304" target='_blank'>
+                        <div className="icon-container">
+                            <Icon name='facebook f' size='large'/>
+                        </div>
+                    </a>
+                    <a href="https://www.youtube.com/channel/UCPmT74NawRaPkpVuVtWYkMA" target='_blank'>
+                        <div className="icon-container">
+                            <Icon name='youtube play' size='large'/>
+                        </div>
+                    </a>   
+                    <a href="https://twitter.com/NSLCanberra" target='_blank'>
+                        <div className="icon-container">
+                            <Icon name='twitter' size='large'/>
+                        </div>
+                    </a> 
+                    <a href="https://www.instagram.com/northsidelegends/" target='_blank'>
+                        <div className="icon-container">
+                            <Icon name='instagram' size='large'/>
+                        </div>
+                    </a>                 
+                </div>
+            </div>
+              
+            <div className="tech-msg-container">
+                <h4>Design & Powered by NSL Tech Team</h4>
+            </div>
         </div>
     )
 }
